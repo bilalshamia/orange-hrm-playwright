47b3c9eed10b085058caf721cf1ae5039e8a0b13
Bilal Shamia <bilalshamia775@gmail.com>
2026-04-02 19:27:44 +0200

    Fix test_example.py imports and NameError

--- tests/e2e/test_example.py
+++ tests/e2e/test_example.py
@@ -1,5 +1,6 @@
 import pytest
 from playwright.sync_api import Page, expect
+from pages.login_page import LoginPage
 def test_valid_login_to_orangehrm(page: Page):
     login_page = LoginPage(page)
     login_page.login_with_valid_admin()
